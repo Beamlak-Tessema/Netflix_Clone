6a438437a9bf22f19b4029f5e5a0c0ef4589cfa5
Beamlak Tessema <beamlaktessemak@gmail.com>
2025-09-07 19:35:18 +0300

    Header and Footer Section added

---
+++ Netflix clone/src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../component/Header/Header";
+import Footer from "../../component/Footer/Footer";
+
+const Hero = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Hero;
