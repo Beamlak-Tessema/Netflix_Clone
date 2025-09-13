94a100de2eb2eb618aaf612f7106dc9958a5baf4
Beamlak Tessema <beamlaktessemak@gmail.com>
2025-09-13 06:21:28 +0300

    deploy

--- Netflix clone/vite.config.js
+++ Netflix clone/vite.config.js
@@ -7,15 +7,9 @@
 // })
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      react: path.resolve("./node_modules/react"),
-    },
-  },
-  base: "Netflix_Clone/Netflix clone/",
+  base: "/Netflix_Clone",
 });
